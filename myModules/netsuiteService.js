1b2104cbcac5b3d05c6358eeabcac2dac3c37d41
Daniel Petrásek <petrasekdan@gmail.com>
2017-03-01 14:00:59 +0100

    Create service object

---
+++ myModules/netsuiteService.js
@@ -0,0 +1,21 @@
+var NetSuiteService = function(){
+    var self = this;
+    
+    self.login = function() {
+
+    }
+
+    self.pushFiles = function(files) {
+
+    }
+
+    self.downloadFiles = function(files) {
+
+    }
+
+    self.downloadFolder = function(folder) {
+        
+    }
+}
+
+module.exports = NetSuiteService;
